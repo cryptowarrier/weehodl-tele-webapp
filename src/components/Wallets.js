dd52868888843b390daa04ecc1503a00ef5d8bde
cryptowarrier <cryptomusha@gmail.com>
2023-05-20 02:46:49 +0900

    updated

--- src/components/Wallets.js
+++ src/components/Wallets.js
@@ -4,15 +4,20 @@ import { getBscBalance, getEthBalance } from "../utils/ethers.util";
 import { getSolBalance } from "../utils/solana.util";
 import { networks } from "../utils/network";
 import { bscChainId, ethChainId } from "../constants";
+import { FaRegCopy } from 'react-icons/fa';
 
 const Wallets = () => {
   const [ethAddress, setEthAddress] = useState('');
   const [solAddress, setSolAddress] = useState('');
-  const [ethBalance, setEthBalance] = useState('');
-  const [bscBalance, setBscBalance] = useState('');
+  const [ethBalance, setEthBalance] = useState('0');
+  const [bscBalance, setBscBalance] = useState('0');
   const [solBalance, setSolBalance] = useState(0);
 
   const [searchParams] = useSearchParams();
+
+  const optimizeAddress = (addr) => {
+    return `${addr.substring(0, 5)}..${addr.substring(addr.length - 5)}`
+  }
 
   useEffect(() => {
     const ethWallet = searchParams.get('ethwallet');
@@ -34,18 +39,18 @@ const Wallets = () => {
     <div>
       <div>
         <div>{`${networks[ethChainId].chainName}`}</div>
-        <div>{`address: ${ethAddress}`}</div>
-        <div>{`balance: ${ethBalance} ETH`}</div>
+        <div>{`address: ${optimizeAddress(ethAddress)}`} <FaRegCopy style={{cursor: 'pointer'}} onClick={() => navigator.clipboard.writeText(ethAddress)} /></div>
+        <div>{`balance: ${Number(ethBalance).toFixed(4)} ETH`}</div>
       </div>
-      <div>
+      <div style={{marginTop: '20px'}}>
         <div>{`${networks[bscChainId].chainName}`}</div>
-        <div>{`address: ${ethAddress}`}</div>
-        <div>{`balance: ${bscBalance} BNB`}</div>
+        <div>{`address: ${optimizeAddress(ethAddress)}`} <FaRegCopy style={{cursor: 'pointer'}} onClick={() => navigator.clipboard.writeText(ethAddress)} /></div>
+        <div>{`balance: ${Number(bscBalance).toFixed(4)} BNB`}</div>
       </div>
-      <div>
+      <div style={{marginTop: '20px'}}>
         <div>{`Solana`}</div>
-        <div>{`address: ${solAddress}`}</div>
-        <div>{`balance: ${solBalance} SOL`}</div>
+        <div>{`address: ${optimizeAddress(solAddress)}`} <FaRegCopy style={{cursor: 'pointer'}} onClick={() => navigator.clipboard.writeText(solAddress)} /></div>
+        <div>{`balance: ${solBalance.toFixed(4)} SOL`}</div>
       </div>
     </div>
   )

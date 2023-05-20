0975dc2ca3f507770376b041c14c6e0712bae2c0
cryptowarrier <cryptomusha@gmail.com>
2023-05-20 13:25:29 +0900

    updated

--- src/components/Wallets.js
+++ src/components/Wallets.js
@@ -5,61 +5,25 @@ import { getSolBalance } from "../utils/solana.util";
 import { networks } from "../utils/network";
 import { bscChainId, ethChainId } from "../constants";
 import { FaRegCopy } from 'react-icons/fa';
-import axios from "axios";
 
 const Wallets = () => {
-  const [ethAddress, setEthAddress] = useState('');
-  const [solAddress, setSolAddress] = useState('');
-  const [ethBalance, setEthBalance] = useState(0);
-  const [bscBalance, setBscBalance] = useState(0);
-  const [solBalance, setSolBalance] = useState(0);
-
+  const [walletAddress, setWalletAddress] = useState('');
+  const [userBalance, setUserBalance] = useState(0);
   const [searchParams] = useSearchParams();
 
   const optimizeAddress = (addr) => {
     return `${addr.substring(0, 5)}..${addr.substring(addr.length - 5)}`
   }
 
-  useEffect(() => {
-    const ethWallet = searchParams.get('ethwallet');
-    const solWallet = searchParams.get('solwallet');
-    setEthAddress(ethWallet);
-    setSolAddress(solWallet);
-    async function getUserBalances() {
-      let balance = await getEthBalance(ethWallet);
-      setEthBalance(balance);
-      balance = await getBscBalance(ethWallet);
-      setBscBalance(balance);
-      balance = await getSolBalance(solWallet);
-      setSolBalance(balance);
-    }
-
-    getUserBalances();
-  }, []);
+  
   return (
     <div>
-      <div>
-        <div>{`${networks[ethChainId].chainName}`}</div>
-        <div>{`address: ${optimizeAddress(ethAddress)}`} <FaRegCopy style={{cursor: 'pointer'}} onClick={() => navigator.clipboard.writeText(ethAddress)} /></div>
-        <div>{`balance: ${Number(ethBalance).toFixed(4)} ETH`}</div>
-      </div>
-      <div style={{marginTop: '20px'}}>
-        <div>{`${networks[bscChainId].chainName}`}</div>
-        <div>{`address: ${optimizeAddress(ethAddress)}`} <FaRegCopy style={{cursor: 'pointer'}} onClick={() => navigator.clipboard.writeText(ethAddress)} /></div>
-        <div>{`balance: ${Number(bscBalance).toFixed(4)} BNB`}</div>
-      </div>
-      <div style={{marginTop: '20px'}}>
-        <div>{`Solana`}</div>
-        <div>{`address: ${optimizeAddress(solAddress)}`} <FaRegCopy style={{cursor: 'pointer'}} onClick={() => navigator.clipboard.writeText(solAddress)} /></div>
-        <div>{`balance: ${solBalance.toFixed(4)} SOL`}</div>
-      </div>
       <div style={{marginTop: '20px'}}>
         <select>
           <option>{networks[ethChainId].chainName}</option>
           <option>{networks[bscChainId].chainName}</option>
           <option>Solana</option>
         </select>
-        <input type="text" />
       </div>
     </div>
   )
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
 export const ethChainId = process.env.REACT_APP_Eth_CHAINID;
 export const bscChainId = process.env.REACT_APP_BSC_CHAINID;
 export const solUrl = process.env.REACT_APP_SOL_URL;
+export const botToken = process.env.REACT_APP_BOT_TOKEN;
+export const secretKey = process.env.REACT_APP_SECRET_KEY;

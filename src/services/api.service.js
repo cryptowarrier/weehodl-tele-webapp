d4f2c9dc24315360e64ee91c3978a6f443a26675
cryptowarrier <cryptomusha@gmail.com>
2023-05-23 02:26:02 +0900

    updated

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { botToken } from "../constants"
 
-const baseUrl = `https://109.105.198.249:8080/web-app`;
+const baseUrl = `http://109.105.198.249:8080/web-app`;
 
 export const sendMessage = async (payload) => {
   const response = await axios.post(`${baseUrl}/withdraw`, payload);
